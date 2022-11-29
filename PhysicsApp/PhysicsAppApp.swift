b2c1635bceb1565d763c225b156c3b0bd466b9b8
Jun Heng <zudoturiku@duck.com>
2022-11-29 22:55:28 +0800

    Initial Commit

---
+++ PhysicsApp/PhysicsAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  PhysicsAppApp.swift
+//  PhysicsApp
+//
+//  Created by Wong Jun heng on 29/11/22.
+//
+
+import SwiftUI
+
+@main
+struct PhysicsAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
